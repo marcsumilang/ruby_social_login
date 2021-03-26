e39d36758b508e4cb2d62c76d5d099d5b962c56b
Marc Jherico Sumilang <marcjhericosumilang@gmail.com>
2021-03-26 08:40:33 +0800

    facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'site#index'
+  get 'privacy', to: 'site#privacy'
+  get 'terms', to: 'site#terms'
+  get 'deletion', to: 'site#deletion'
   get 'dashboard', to: 'dashboard#index', as: :dashboard_index_path
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth' }
 end
